765eb2fc8d7cafb1241801ea8ca499d66ff6bc3c
BekaHabesha <bereketdm1984@gmail.com>
2024-04-20 19:39:43 -0700

    No.0. Sky is the limit, let's bring that limit higher.

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+# Puppet script to adjust the OS configuration for the holberton user
+
+exec { 'fix--for-nginx':
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
+
+exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
