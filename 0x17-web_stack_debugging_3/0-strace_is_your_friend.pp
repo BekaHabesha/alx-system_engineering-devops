073cb838f58027284e63a310af99125f5ae7b7b7
BekaHabesha <bereketdm1984@gmail.com>
2024-04-24 14:27:14 -0700

    No.0.Strace is your friend.

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Using strace, find out why Apache is returning a 500 error, then Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+# Using strace, find out why Apache is returning a 500 error, then Fixes bad phpp extensions to `php` in the WordPress file `wp-settings.php`.
 
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
