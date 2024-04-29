26c97c6d431fb85d34b4ef8c3a5f8ddb60639a54
BekaHabesha <bereketdm1984@gmail.com>
2024-04-28 17:07:44 -0700

    No.0.How many subs.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and returns the number of subscribers for a given subreddit.
-If the subreddit is invalid, it returns 0.
-"""
-
+""" Module for task 0 """
 import requests
 
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "Linux:0x16.api.advanced:v1.0.0 (by /u/bekahabesha)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'reddit_api:project:\(by /u/bekahabesha)'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        else:
+            return 0
+    except requests.exceptions.RequestException as e:
+        print("An error occurred:", e)
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
